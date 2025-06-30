94c432c3c03a7fa432c9e3550f3f162c3f2d5828
poonam taxak <poonamtaxak04@gmail.com>
2025-06-30 16:29:39 +0530

    Add files via upload

---
+++ SportAndSoccerOverriden.java
@@ -0,0 +1,37 @@
+
+
+class Sports {
+
+    String getName() {
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has n players in " + getName());
+    }
+}
+
+class Soccer extends Sports {
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers() {
+        // Overridden to print the number of players for Soccer
+        System.out.println("Each team has 11 players in " + getName());
+    }
+}
+
+public class SportAndSoccerOverriden {
+
+    public static void main(String[] args) {
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+}
